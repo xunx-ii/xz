447dc2ffc49c6af214622385d8f6e7990f3bf325
xic <xic@live.com>
2025-03-20 06:02:59 +0000

    新增onnxruntime

--- src/application.cpp
+++ src/application.cpp
@@ -1,48 +1,45 @@
 #include "application.h"
 
 #include "nlohmann/json.hpp"
+#include "onnxruntime_cxx_api.h"
 #include "spdlog/spdlog.h"
 
+#include <optional>
 #include <thread>
 
-application::application() {
-    should_stop_server = false;
-    
-}
+application::application() { should_stop_server = false; }
 
-application::~application() { 
-
-    mqtt_thread.join(); 
-}
+application::~application() { mqtt_thread.join(); }
 
 int application::run() {
 	// 先启动一下OTA服务器 OTA服务器在异步跑
 	ota_server = new ota();
 	ota_server->listen();
 
-	mqtt_thread = std::thread([this]() 
-        { 
-            spdlog::info("mqtt thread start!");
-            connect_mqtt_server(); 
-        });
+	mqtt_thread = std::thread([this]() {
+		spdlog::info("mqtt thread start!");
+		connect_mqtt_server();
+	});
 
-    main_loop();
+	spdlog::info("Ort Version: {0}", Ort::GetVersionString());
+
+	Ort::SessionOptions session_options;
+	session_options.SetIntraOpNumThreads(1);
+
+	main_loop();
 
 	return 0;
 }
 
-void application::process(mqtt_message_t *message)
-{
-
-}
+void application::process(mqtt_message_t *message) {}
 
 void application::main_loop() {
-    while (!should_stop_server) {
-        auto val = rev_safe_queue.dequeue();
-        if(val.has_value()){
-            process(val.value());
-        }
-    }
+	while (!should_stop_server) {
+		std::optional<mqtt_message_t *> val = rev_safe_queue.dequeue();
+		if (val.has_value()) {
+			process(val.value());
+		}
+	}
 }
 
 void application::connect_mqtt_server() {
@@ -52,9 +49,8 @@ void application::connect_mqtt_server() {
 
 	};
 
-	mqtt_client.onMessage = ([this](hv::MqttClient *client, mqtt_message_t *message) {
-        rev_safe_queue.enqueue(message);
-	});
+	mqtt_client.onMessage =
+	    ([this](hv::MqttClient *client, mqtt_message_t *message) { rev_safe_queue.enqueue(message); });
 
 	mqtt_client.onClose = [this](hv::MqttClient *client) {
 
--- src/ota.cpp
+++ src/ota.cpp
@@ -1,30 +1,48 @@
 #include "ota.h"
 #include "hv/HttpServer.h"
-
+#include "nlohmann/json.hpp"
 #include "spdlog/spdlog.h"
+#include <string>
 
-ota::ota()
-{
-    listen_port = 41300;
-}
+ota::ota() { listen_port = 3000; }
 
-ota::~ota()
-{
+ota::~ota() {}
 
-}
+void ota::listen(int port, int worker_threads) {
+	listen_port = port;
 
-void ota::listen(int port, int worker_threads)
-{
-    listen_port = port;
+	ota_service.GET("/", [this](HttpRequest *req, HttpResponse *resp) {
+		nlohmann::json ex1 = nlohmann::json::parse(R"(
+        "mqtt": {
+            "endpoint": "post-cn-apg3xckag01.mqtt.aliyuncs.com",
+            "client_id": "GID_test@@@88_ae_dd_90_50_98",
+            "username": "Signature|LTAI5tF8J3CrdWmRiuTjxHbF|post-cn-apg3xckag01",
+            "password": "A36Z1YCv3yPb7MIGOoMHMli/FI4=",
+            "publish_topic": "device-server",
+            "subscribe_topic": "devices/88_ae_dd_90_50_98"
+        },
+        "server_time": {
+            "timestamp": 1742440674422,
+            "timezone_offset": 480
+        },
+        "firmware": {
+            "version": "1.4.6",
+            "url": "https://xiaozhi-voice-assistant.oss-cn-shenzhen.aliyuncs.com/firmwares/v1.4.6_bread-compact-wifi/xiaozhi.bin"
+        },
+        "activation": {
+            "code": "702334",
+            "message": "xiaozhi.me\n702334"
+        }
+        )");
+        
+		std::string data_str = ex1.dump().c_str();
+        
+		return resp->Data((void*)data_str.c_str(), data_str.length());
+	});
 
-    ota_service.GET("/ota", [](HttpRequest* req, HttpResponse* resp) {
-        static char data[] = "0123456789";
-        return resp->Data(data, 10);
-    });
-
-    ota_server.port = port;
-    ota_server.service = &ota_service;
-    ota_server.worker_threads = worker_threads;
-    spdlog::info("ota server start!");
-    http_server_run(&ota_server, 0);
+	ota_server.port = port;
+	ota_server.service = &ota_service;
+	ota_server.worker_threads = worker_threads;
+	spdlog::info("ota server start!");
+	http_server_run(&ota_server, 0);
 }
